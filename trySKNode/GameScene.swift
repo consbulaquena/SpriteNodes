bac673ae1ec0e523350fcb9d4e8f273e2696b42f
Cons Bulaqueña <consbulaquena@gmail.com>
2017-04-29 21:29:17 +0800

    Add files via upload

---
+++ trySKNode/GameScene.swift
@@ -0,0 +1,31 @@
+//
+//  GameScene.swift
+//  trySKNode
+//
+//  Created by Cons Bulaqueña on 27/04/2017.
+//  Copyright © 2017 consios. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+        
+        <#code#>
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Called when a touch begins
+        
+    }
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+
+        }
+}
